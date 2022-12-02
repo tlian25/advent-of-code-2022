2a9ceb00fcd359b2498cb59c7d2797f324f1ca48
txlian <txlian@amazon.com>
2022-12-02 14:13:21 -0500

    day 2

--- solutions/day02_rock_paper_scissors.py
+++ solutions/day02_rock_paper_scissors.py
@@ -9,11 +9,99 @@ def readfile():
 
 
 
-def solution():
-    pass
+# CONSTANTS
+LOST = 0
+DRAW = 3
+WON = 6
+
+ROCK = 'X'
+PAPER = 'Y'
+SCISSOR = 'Z'
+
+score_mapping = {ROCK: 1, PAPER: 2, SCISSOR: 3}
+
+def convert(op):
+    mapping = {'A': ROCK, 'B': PAPER, 'C': SCISSOR}
+    return mapping[op]
+
+def score(op, you):
+    score = score_mapping[you]
+    
+    if op == you:
+        score += DRAW
+    
+    elif op == ROCK and you == PAPER:
+        score += WON
+    
+    elif op == SCISSOR and you == ROCK:
+        score += WON
+    
+    elif op == PAPER and you == SCISSOR:
+        score += WON
+    
+    return score
+     
+
+
+def solution1():
+    
+    lines = readfile()
+    
+    total = 0
+    for l in lines:
+        op, you = l.replace('\n','').split(' ')
+        op = convert(op)
+        s = score(op, you)
+        total += s
+        print(op, you, s)
+        
+    return total
+
+
+#################################
+
+
+def should_play_to_win(op):
+    if op == ROCK: return PAPER
+    if op == PAPER: return SCISSOR
+    return ROCK
+    
+
+def should_play_to_lose(op):
+    if op == ROCK: return SCISSOR
+    if op == PAPER: return ROCK
+    return PAPER
+
+def should_play_to_draw(op):
+    return op
+
+
+strategy_mapping = {'X': should_play_to_lose, 'Y': should_play_to_draw, 'Z': should_play_to_win}
+
+def solution2():
+    
+    total = 0
+    
+    lines = readfile()
+    for l in lines:
+        op, strat = l.replace('\n', '').split(' ')
+        op = convert(op)
+        strategy = strategy_mapping[strat]
+        
+        play = strategy(op)
+        s = score(op, play)
+        
+        total += s
+        
+    return total
+        
+    
+    
+    
 
 
 
 
 if __name__ == '__main__':
-    print(solution)
+    print(solution1())
+    print(solution2())
